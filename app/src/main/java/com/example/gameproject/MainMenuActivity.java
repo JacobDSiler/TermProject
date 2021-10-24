8758d58bbf838e7b07d9d323d976fc97e66d4c07
Ben Ellis <ben.ellis1919@gmail.com>
2021-10-23 21:22:39 -0600

    Added activities and backgrounds to each

---
+++ app/src/main/java/com/example/gameproject/MainMenuActivity.java
@@ -0,0 +1,30 @@
+package com.example.gameproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openSettingsActivity(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openHighScoresActivity(View view) {
+        Intent intent = new Intent(this, HighScoresActivity.class);
+        startActivity(intent);
+    }
+
+    public void openGameActivity(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+}
