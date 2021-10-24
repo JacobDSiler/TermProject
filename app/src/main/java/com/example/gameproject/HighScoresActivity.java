881c2a42eb9fe984c56c226922c11b8d1c874b56
Ben Ellis <ben.ellis1919@gmail.com>
2021-10-23 23:39:38 -0600

    Updates

--- app/src/main/java/com/example/gameproject/HighScoresActivity.java
+++ app/src/main/java/com/example/gameproject/HighScoresActivity.java
@@ -2,7 +2,9 @@ package com.example.gameproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HighScoresActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class HighScoresActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_scores);
+    }
+
+    public void returnToMainMenuActivity(View view) {
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
     }
 }
