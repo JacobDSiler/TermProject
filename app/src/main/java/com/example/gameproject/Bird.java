b3862030fb1e4845565dd6e18ca11c53848829aa
Jacob Siler <46026740+JacobDSiler@users.noreply.github.com>
2021-10-28 15:58:16 +0100

    Add Object class, and Bird class, integrate Draw infrastructure

---
+++ app/src/main/java/com/example/gameproject/Bird.java
@@ -0,0 +1,32 @@
+package com.example.gameproject;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+import java.util.ArrayList;
+
+public class Bird extends BaseObject {
+    private ArrayList<Bitmap> arrBms = new ArrayList<>();
+    public Bird() {
+
+    }
+    public void draw(Canvas canvas){
+        canvas.drawBitmap(this.getBm(), this.x, this.y, null);
+    }
+
+    public ArrayList<Bitmap> getArrBms() {
+        return arrBms;
+    }
+
+    public void setArrBms(ArrayList<Bitmap> arrBms) {
+        this.arrBms = arrBms;
+        for (int i = 0; i< arrBms.size(); i++) {
+            this.arrBms.set(i, Bitmap.createScaledBitmap(this.arrBms.get(i), this.width, this.height, true));
+        }
+    }
+
+    @Override
+    public Bitmap getBm() {
+        return this.getArrBms().get(0);
+    }
+}
--- app/src/main/java/com/example/gameproject/GameView.java
+++ app/src/main/java/com/example/gameproject/GameView.java
@@ -1,17 +1,34 @@
 package com.example.gameproject;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+
 public class GameView extends View {
+    private Bird bird;
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        bird = new Bird();
+        bird.setWidth(128);
+        bird.setHeight(128);
+        bird.setX(100);
+        bird.setY(500-bird.getHeight());
+        ArrayList<Bitmap> arrBms = new ArrayList<>();
+        arrBms.add(BitmapFactory.decodeResource(this.getResources(), R.drawable.frame_1));
+        arrBms.add(BitmapFactory.decodeResource(this.getResources(), R.drawable.frame_2));
+        arrBms.add(BitmapFactory.decodeResource(this.getResources(), R.drawable.frame_3));
+        arrBms.add(BitmapFactory.decodeResource(this.getResources(), R.drawable.frame_4));
+        bird.setArrBms(arrBms);
     }
     public void draw(Canvas canvas) {
         super.draw(canvas);
+        bird.draw(canvas);
     }
 }
