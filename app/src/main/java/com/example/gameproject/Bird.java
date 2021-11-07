c4ebcab643f9f26dab543eefddfd71fee3d8a398
Samuel Krohn <SANIKR@outlook.com>
2021-11-06 20:35:02 -0600

    Bird flapping, Drop, angle, and onTouchEvent

--- app/src/main/java/com/example/gameproject/Bird.java
+++ app/src/main/java/com/example/gameproject/Bird.java
@@ -2,16 +2,20 @@ package com.example.gameproject;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 
 import java.util.ArrayList;
 
 public class Bird extends BaseObject {
     private ArrayList<Bitmap> arrBms = new ArrayList<>();
-    public Bird() {
+    private int count, vFlap, idCurrentBitmap;
+    private float drop;
 
-    }
-    public void draw(Canvas canvas){
-        canvas.drawBitmap(this.getBm(), this.x, this.y, null);
+    public Bird() {
+        this.count = 0;
+        this.vFlap = 5;
+        this.idCurrentBitmap = 0;
+        this.drop = 0;
     }
 
     public ArrayList<Bitmap> getArrBms() {
@@ -25,8 +29,64 @@ public class Bird extends BaseObject {
         }
     }
 
+    public void setDrop(float drop) {
+        this.drop = drop;
+    }
+
     @Override
     public Bitmap getBm() {
-        return this.getArrBms().get(0);
+        count++;
+        if (this.count == this.vFlap) {
+            for (int i = 0; i < arrBms.size(); i++) {
+                if(i == arrBms.size()-1) {
+                    this.idCurrentBitmap = 0;
+                    break;
+                } else if (this.idCurrentBitmap == i) {
+                    idCurrentBitmap = i+1;
+                    break;
+                }
+            }
+            count = 0;
+        }
+        if (this.drop < 0){
+            Matrix matrix = new Matrix();
+            matrix.postRotate(-25);
+            return Bitmap.createBitmap(
+                    arrBms.get(idCurrentBitmap),
+                    0,
+                    0,
+                    arrBms.get(idCurrentBitmap).getWidth(),
+                    arrBms.get(idCurrentBitmap).getHeight(),
+                    matrix,
+                    true
+            );
+        } else if (drop >= 0){
+            Matrix matrix = new Matrix();
+            if (drop < 70) {
+                matrix.postRotate(-25+(drop*2));
+            } else {
+                matrix.postRotate(45);
+            }
+            return Bitmap.createBitmap(
+                    arrBms.get(idCurrentBitmap),
+                    0,
+                    0,
+                    arrBms.get(idCurrentBitmap).getWidth(),
+                    arrBms.get(idCurrentBitmap).getHeight(),
+                    matrix,
+                    true
+            );
+        }
+        return this.arrBms.get(idCurrentBitmap);
+    }
+
+    public void draw(Canvas canvas){
+        drop();
+        canvas.drawBitmap(this.getBm(), this.x, this.y, null);
+    }
+
+    private void drop() {
+        this.drop+=0.6;
+        this.y+=this.drop;
     }
 }
--- app/src/main/java/com/example/gameproject/GameView.java
+++ app/src/main/java/com/example/gameproject/GameView.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.os.Handler;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -13,6 +15,9 @@ import java.util.ArrayList;
 
 public class GameView extends View {
     private Bird bird;
+    private Handler handler;
+    private Runnable r;
+
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         bird = new Bird();
@@ -26,9 +31,20 @@ public class GameView extends View {
         arrBms.add(BitmapFactory.decodeResource(this.getResources(), R.drawable.frame_3));
         arrBms.add(BitmapFactory.decodeResource(this.getResources(), R.drawable.frame_4));
         bird.setArrBms(arrBms);
+        handler = new Handler();
+        r = this::invalidate;
     }
     public void draw(Canvas canvas) {
         super.draw(canvas);
         bird.draw(canvas);
+        handler.postDelayed(r, 10);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction()== MotionEvent.ACTION_DOWN) {
+            bird.setDrop(-15);
+        }
+        return true;
     }
 }
